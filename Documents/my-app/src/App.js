d84aa3febf73e72c0386d199c7c4dc50bee59b92
Mohamed Amine <mohamedaminebenhmida4@gmail.com>
2022-09-14 15:52:07 +0200

    first-commit

---
+++ Documents/my-app/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/Navbar';
+import MyForm from './components/MyForm';
+
+function App() {
+  return (
+
+    <div className="App">
+      <NavBar/>
+      <MyForm/>
+    </div>
+
+
+  );
+
+}
+
+export default App;
+
